618fb0abe2991851a38be9bd5c90d0f0f58f01c9
jithinsdileep12 <89510784+jithinsdileep12@users.noreply.github.com>
2021-12-23 19:51:12 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/kjmods"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/kjmods'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/jithinsdileep12/KJBOTV2')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/kjmods0707')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
